50a985c7019a16c2092e9a8d71845d7206579216
Ubuntu <marcais.jules@gmail.com>
2022-12-12 02:28:16 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
+source /var/lib/miniconda3/etc/profile.d/conda.sh
 conda activate snakemake
 conda install -y -c conda-forge singularity
-snakemake -s rules/getFastq --cores 8 --use-singularity
+snakemake -s rules/getFastq --cores 1 --use-singularity
 snakemake -s rules/getGenomes --cores 8
 snakemake -s rules/indexGenome --cores 8 --use-singularity --singularity-args "--bind ~/Repro_Hackaton"
 snakemake -s rules/mapping --cores 8 --use-singularity
